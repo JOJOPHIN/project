84d1a9e510215400fc78c35f54a24042b8d1eb7b
JO <77269800+JOJOPHIN@users.noreply.github.com>
2021-01-18 17:44:37 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,5 +1,8 @@
- <?php
- 
+<?php
+
+if(isset($_POST['submit']))
+{
+     
 $firstname=$_POST['firstname'];
 $lastname=$_POST['lastname'];
 $housename=$_POST['housename'];
@@ -15,45 +18,34 @@ $busroute=$_POST['busroute'];
 $phoneno=$_POST['phoneno'];
 $fees=$_POST['fees'];
 
-if.(!empty($firstname) || !empty($lastname) || !empty($housename) || !empty($locality) || !empty($district) || !empty($pincode) || !empty($branch) || !empty($year) || !empty($registrationno) || !empty($pickuppoint) || !empty($droppoint) || !empty($busroute) || !empty($phoneno) || !empty($fees))
+
+$con=new mysqli("localhost","root","","register");
+
+
+
+if(!empty($firstname) || !empty($lastname) || !empty($housename) || !empty($locality) || !empty($district) || !empty($pincode) || !empty($branch) || !empty($year) || !empty($registrationno) || !empty($pickuppoint) || !empty($droppoint) || !empty($busroute) || !empty($phoneno) || !empty($fees))
 {
-    $host="localhost";
-    $dbusername="root";
-    $dbpassword="";
-    $dbname="register";
-//craete connection
-$conn= new mysqli($host, $dbusername, $dbpassword, $dbname);
-if (mysqli_connect_error()){
-    die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-}else{
-    $SELECT = "SELECT phoneno From student where phoneno=? Limit 1";
-    $INSERT = "INSERT Into student (firstname, lastname, housename, locality, district, pincode, branch, year, registrationno, pickuppoint, droppoint, busroute, phoneno, fees) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-   //prepare statement
-   $stmt = $conn->prepare($SELECT);
-   $stmt->bind_param("i",$phoneno);
-   $stmt->execute();
-   $stmt->bind_result($phoneno);
-   $stmt->store_result();
-   $rnum = $stmt->num rows;
+     $sql="insert into student(firstname,lastname,housename,locality,district,pincode,branch,year,registrationno,pickuppoint,droppoint,busroute,phoneno,fees) values('$firstname','$lastname','$housename','$locality','$district','$pincode','$branch','$year','$registrationno','$pickuppoint','$droppoint','$busroute','$phoneno','$fees')";
+     if($con->query($sql)==TRUE)
+    {
+       echo "Your account has been created";
 
-   if ($rnum==0) {
-       $stmt->close();
+    } 
+    else
+    {
+       echo " ".$con->error;
+    }
 
-       $stmt = $conn->prepare($INSERT);
-       $stmt->bind_param("ssssii", $firstname, $lastname, $housename, $locality, $district, $pincode, $branch, $year, $registrationno, $pickuppoint, $droppoint, $busroute, $phoneno, $fees);
-       $stmt->execute();
-       echo "New record inserted successfully";
-}else{
-    echo "SOMEONE ALREADY REGISTERED USING THIS PHONE NO";
 }
-$stmt->close();
-$conn->close();
+else
+{
+    echo "Please enter all the required fields";
+}
+
 
 }
 
-}else {
-    echo"All Fields Are Required";
-    die();
-} 
+
+
 
 ?>
